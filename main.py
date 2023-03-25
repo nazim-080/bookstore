2c8dac9c87ace83b988c7024116243e4984c1e4a
nazim <nazimlay15@gmail.com>
2023-03-25 23:56:30 +0300

    configured automatic upgrade of migrations

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
-
-from config import settings
+from models.db import run_async_upgrade
 
 app = FastAPI(title="Bookstore")
+
+
+@app.on_event("startup")
+async def startup() -> None:
+    """Startup application."""
+
+    await run_async_upgrade()
 
 
 @app.get("/")
 def hello():
     return "Hello World"
-
